30197bea12de53b88843ea9dd9edc5a24a1c0069
Tuan Hung <nguyentuanhung4797@gmail.com>
2018-08-22 08:25:08 +0700

    add files

---
+++ NegendoTx.cpp
@@ -0,0 +1,95 @@
+#include "NegendoTx.h"
+#include "RF24.h"
+#include "EEPROM.h"
+
+void NegendoTx::init()
+{
+	Serial.begin(9600);
+	randomSeed(analogRead(A7));
+
+	pinMode(SET, OUTPUT);
+	pinMode(BF, INPUT);
+	pinMode(BB, INPUT);
+	pinMode(BR, INPUT);
+	pinMode(BL, INPUT);
+	pinMode(BLed, INPUT);
+	pinMode(BX, INPUT);
+	pinMode(BSP, INPUT);
+	pinMode(Led,OUTPUT);
+
+	radio.begin();
+	radio.setChannel(108);
+	radio.setDataRate(RF24_1MBPS);
+	radio.setPALevel(RF24_PA_HIGH);
+	convertAdd();
+}
+void NegendoTx::convertAdd()
+{
+	_readAdd = EEPROM.read(0);
+	_AddRandom = (_AddDefault & ~0xFFLL) | _readAdd;
+	Serial.print("Address: ");
+	Serial.print((unsigned long)(_AddRandom >> 32), 16);
+	Serial.println((unsigned long)_AddRandom, 16);
+	radio.openWritingPipe(_AddRandom);
+}
+void NegendoTx::setAddress()
+{
+	if(!digitalRead(SET))
+	{
+		Serial.println("Set Address");
+		Serial.println("Wait 5s...");
+		_startTime = millis();
+		while(!digitalRead(SET));
+		_duration = millis() - _startTime;
+		if(_duration > 5000)
+		{
+			//Serial.println("Ready to send new address");
+			_address = random(2,255);
+			EEPROM.write(0,_address);
+			_Add[0] = _address;
+			radio.openWritingPipe(_AddDefault);
+			radio.write(_Add, sizeof(_Add));
+			Serial.println("Set address done.");
+			convertAdd();
+			tick(3,1000,100);
+			blinks(3,100);
+			delay(1000);
+		}
+		else
+		{
+			tick(2,1000,400);
+			blinks(2,400);
+		}
+	}
+}
+void NegendoTx::blinks(int n, int times)
+{
+	for(int i=0; i<n; i++)
+	{
+		digitalWrite(Led, HIGH);
+		delay(times);
+		digitalWrite(Led, LOW);
+		delay(times);
+	}
+}
+void NegendoTx::tone(uint16_t frequency, uint32_t duration)
+{
+  int period = 1000000L / frequency;
+  int pulse = period / 2;
+  for (long i = 0; i < duration * 1000L; i += period)
+  {
+    digitalWrite(buzzer, HIGH);
+    delayMicroseconds(pulse);
+    digitalWrite(buzzer, LOW);
+    delayMicroseconds(pulse);
+  }
+}
+void NegendoTx::tick(int n, uint16_t frequency, int times)
+{
+	for(int i=0; i<n; i++)
+	{
+		tone(frequency, times);
+		digitalWrite(buzzer, LOW);
+		delay(times);
+	}
+}
---
+++ NegendoTx.h
@@ -0,0 +1,52 @@
+#ifndef NegendoTx_h
+#define NegendoTx_h
+
+#include "RF24.h"
+#include <SPI.h>
+#include <EEPROM.h>
+
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#define CE_PIN   A2
+#define CSN_PIN A3
+
+#define SET 2
+#define BF  3
+#define BB  4
+#define BR  5
+#define BL  6
+#define BLed  7
+#define BX  8
+#define BSP 9
+#define Speed A0
+#define buzzer A1
+#define Led 13
+
+class NegendoTx
+{
+public:
+	NegendoTx(){}
+
+	void init();
+	void convertAdd();
+	void setAddress();
+	void blinks(int n, int times);
+	void tone(uint16_t frequency, uint32_t duration);
+	void tick(int n, uint16_t frequency, int times);
+private:
+	const uint64_t _AddDefault = 0xF0F0F0F001LL;
+	uint64_t _AddRandom;
+	byte _readAdd;
+	byte _address;
+	int _Pot;
+	int _Add[1];
+	long _duration;
+	long _startTime;
+
+	RF24 radio= RF24(CE_PIN, CSN_PIN);
+};
+#endif 
